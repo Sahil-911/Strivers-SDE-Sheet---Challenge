6472bc885d85864095e2463f6fed2d51b5110336
Sahil <sahillakdawala1109@gmail.com>
2023-06-04 12:42:14 +0530

    first commit

---
+++ 1_set_matrix_zeroes.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setZeros(vector<vector<int>> &matrix)
+{
+	unordered_set<int> rows,cols;
+	for(int i=0;i<matrix.size();i++)
+	{
+		for(int j=0;j<matrix[0].size();j++)
+		{
+			if(matrix[i][j]==0)
+			{
+				rows.insert(i);
+				cols.insert(j);
+			}
+		}
+	}
+	for(int i=0;i<matrix.size();i++)
+	{
+		for(int j=0;j<matrix[0].size();j++)
+		{
+			if(rows.count(i) || cols.count(j))
+			{
+				matrix[i][j]=0;
+			}
+		}
+	}
+}
